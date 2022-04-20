514f2dd66bd140a2a136d036618a11b72047f8d0
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2022-04-20 07:44:00 +0300

    changed udz1_2.py for better illustration

--- udz1/udz1_2.py
+++ udz1/udz1_2.py
@@ -7,3 +7,10 @@ class Soda:
             print(f"Газировка и {self.flavour}")
         else:
             print("Обычная газировка")
+
+
+regular = Soda()  # flavour=None
+regular.show_my_drink()  # Обычная газировка
+
+fanta = Soda("апельсин")  # flavour="апельсин"
+regular.show_my_drink()  # Газировка и апельсин
