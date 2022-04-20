e53881fdbf09bbfa64de5287fcffd2414ab34940
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2022-04-20 08:11:33 +0300

    Updated udz1_3.py for better illustration

--- udz1/udz1_3.py
+++ udz1/udz1_3.py
@@ -16,8 +16,15 @@ class Math1:
         print(self.a / self.b)
 
 
+math1 = Math1(6, 3)
+math1.addition()  # 9
+math1.substraction()  # 3
+math1.multiplication()  # 18
+math1.division()  # 2.0
+
+
 class Math2:
-    def __init__(self) -> None:
+    def __init__(self):
         pass
 
     @staticmethod
@@ -35,3 +42,9 @@ class Math2:
     @staticmethod
     def division(a, b):
         print(a / b)
+
+
+Math2.addition(6, 3)  # 9
+Math2.substraction(6, 3)  # 3
+Math2.multiplication(6, 3)  # 18
+Math2.division(6, 3)  # 2.0
